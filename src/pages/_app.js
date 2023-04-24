c415c3102e3b348fcbfae7464893168311c7886c
Rega HR <rega.hruzty@gmail.com>
2023-04-25 01:30:46 +0700

    init project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    aos.init();
+  }, []); 
   return <Component {...pageProps} />
 }
